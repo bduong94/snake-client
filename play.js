721f8525159b873ac46d17c3e9c96cc1749bda0a
Brian Duong <brianminhduong@gmail.com>
2021-11-17 03:12:02 +0000

    Move setupInput function to input.js file

--- play.js
+++ play.js
@@ -1,24 +1,8 @@
 const net = require('net');
-const conn = require('./client.js');
-const handleUserInput = function() {
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    };
-    // process.stdout.write(key);
-  });
-};
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on('data', handleUserInput);
-  return stdin;
-};
+const { connect }= require('./client.js');
+const { setupInput }= require('./input');
 
 console.log("Connecting ...");
 
-conn.connect();
+connect();
 setupInput();
