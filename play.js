aa9abf541175ce0ac5d48edd104e9881193275b9
Brian Duong <90289006+bduong94@users.noreply.github.com>
2021-11-16 19:01:35 -0500

    Create client.js file and move connect file to it

--- play.js
+++ play.js
@@ -1,19 +1,4 @@
-const net = require('net');
-
-const connect = function() {
-  const conn = net.createConnection({
-    host:'10.0.2.15',
-    port:50541
-  });
-
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-
-  conn.setEncoding('utf8');
-
-  return conn;
-};
+const conn = require('./client.js');
 
 console.log("Connecting ...");
-connect();
+conn.connect();
