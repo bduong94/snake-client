438497fe49ab38f7ee193d0c7aac658c210f0cf0
Brian Duong <brianminhduong@gmail.com>
2021-11-17 03:05:27 +0000

    Add keyboard input into play.js

--- play.js
+++ play.js
@@ -1,4 +1,24 @@
+const net = require('net');
 const conn = require('./client.js');
+const handleUserInput = function() {
+  process.stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    };
+    // process.stdout.write(key);
+  });
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+};
 
 console.log("Connecting ...");
+
 conn.connect();
+setupInput();
